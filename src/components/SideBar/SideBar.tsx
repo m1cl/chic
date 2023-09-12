f2c9454971ee474d2013a50a740ab6b3a35c5e67
Michael Ajala <m1cl@MBP-von-Michael.fritz.box>
2023-09-12 12:02:18 +0200

    Fix height and width

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,49 +1,54 @@
-import React, { useState } from "react";
-import { Link as L } from "react-router-dom";
+import React, {useState} from "react";
+import {Link as L} from "react-router-dom";
 import styled from "styled-components";
-import { black, grey } from "../../colors";
-import { useStore } from "../../store";
-import { PlaylistType } from "../../types";
+import {black, grey} from "../../colors";
+import {useStore} from "../../store";
+import {PlaylistType} from "../../types";
 
 const Container = styled.div`
-  display: flex;
-  background-color: ${black};
-  height: 100%;
-  padding: 80px 15px 80px 15px;
-  min-width: 240px;
-  max-width: 30vw;
+display: flex;
+background-color: ${black};
+height: 1080px;
+padding: 80px 15px 80px 15px;
+min-width: 240px;
+width: 15vw;
+max-width: 15vw;
+overflow-y:scroll;
+overflow-x: scroll;
+min-height:0;
 `;
 const Main = styled.div`
-  background-color: #000000;
-  display: flex;
-  justify-content: space-around;
-  height: 1920;
+background-color: #000000;
+display: flex;
+justify-content: space-around;
+height: 1920;
 `;
 
 const MenuContainer = styled.ul`
-  color: ${grey};
+color: ${grey};
 `;
 const MenuContent = styled.div`
-  margin-bottom: 80px;
+margin-bottom: 80px;
 `;
 
 const MenuItem = styled.li`
-  list-style: none;
-  margin-left: 22px;
-  margin-bottom: 14px;
+list-style: none;
+margin-left: 22px;
+margin-bottom: 14px;
 `;
 
 const Link = styled(L)`
-  text-decoration: none;
-  color: grey;
-  &:hover {
-    cursor: pointer;
-    color: white;
-  }
+text-decoration: none;
+color: grey;
+&:hover {
+  cursor: pointer;
+  color: white;
+}
 `;
 const SideBar = () => {
   const [_touchStart, setTouchStart] = useState(null);
   const [_touchEnd, setTouchEnd] = useState(null);
+  const [showPlaylistNav, setShowPlaylistNav] = useState(true);
 
   // the required distance between touchStart and touchEnd to be detected as a swipe
   // const minSwipeDistance = 10;
@@ -104,9 +109,9 @@ const SideBar = () => {
               <Link to="/artists">Artists</Link>
             </MenuItem>
           </MenuContent>
-          <MenuContent>
-            <h3>Playlists</h3>
-            {MenuItems}
+          <MenuContent onClick={() => setShowPlaylistNav(!showPlaylistNav)}>
+            <h3>{showPlaylistNav ? "" : "..."}Playlists</h3>
+            {showPlaylistNav && MenuItems}
           </MenuContent>
         </MenuContainer>
       </Container>
