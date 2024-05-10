3bccd1bc1d259e06849ee238c3f68b328cb1deca
Michael Ajala <m1cl@MacBook-Pro-von-Michael.local>
2024-05-10 16:17:09 +0200

    Add dynamic component and disable ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image';
+import Playlist from '@/components/playlist';
+import dynamic from 'next/dynamic';
 
+dynamic(() => import('@/components/playlist'), { ssr: false });
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Playlist />
+    </main>
   );
 }
