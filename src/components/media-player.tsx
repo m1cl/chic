e4f252823fbaffe1fc0bb80f7c76387da8ff6b84
Michael Ajala <m1cl@MacBook-Pro-von-Michael.local>
2024-05-10 16:17:13 +0200

    refactor

--- src/components/media-player.tsx
+++ src/components/media-player.tsx
@@ -1,17 +1,11 @@
 'use client';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-import { useStore } from '../store';
-import { useEffect, useState } from 'react';
 
 export default function MediaPlayer() {
   return (
     <div className="p-auto fixed bottom-0 mx-auto my-auto flex h-18 w-full justify-center bg-grey-300">
-      <AudioPlayer
-        autoPlay
-        src={''}
-        // other props here
-      />
+      <AudioPlayer autoPlay src={''} />
     </div>
   );
 }
