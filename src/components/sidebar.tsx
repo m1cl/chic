201b4a2674ab2ef5eff47df4ffd904c4f7d14cd0
Michael Ajala <m1cl@MacBook-Pro-von-Michael.local>
2024-05-10 16:16:48 +0200

    Remove unimplemented paths

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,12 +1,12 @@
-'use client';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const links = [
-  { href: '/', label: 'Home' },
-  { href: '/artists', label: 'Artists' },
-  { href: '/playlists', label: 'Playlist' },
+  { href: "/", label: "Home" },
+  // { href: '/artists', label: 'Artists' },
+  // { href: '/playlists', label: 'Playlist' },
 ];
 
 export default function Sihebar() {
@@ -20,9 +20,9 @@ export default function Sihebar() {
           role="button"
           tabIndex={0}
           className={clsx(
-            'flex w-full items-center rounded-lg p-3 text-start leading-tight outline-none transition-all hover:bg-blue-50 hover:bg-opacity-80 hover:text-blue-900 focus:bg-blue-50 focus:bg-opacity-80 focus:text-blue-900 active:bg-gray-50 active:bg-opacity-80 active:text-blue-900',
+            "flex w-full items-center rounded-lg p-3 text-start leading-tight outline-none transition-all hover:bg-blue-50 hover:bg-opacity-80 hover:text-blue-900 focus:bg-blue-50 focus:bg-opacity-80 focus:text-blue-900 active:bg-gray-50 active:bg-opacity-80 active:text-blue-900",
             {
-              'bg-sky-100 text-blue-600': pathname === href,
+              "bg-sky-100 text-blue-600": pathname === href,
             },
           )}
         >
