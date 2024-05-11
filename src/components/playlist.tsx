1faa8ca9077e5fac948959d4a11151e342d619c7
Michael Ajala <m1cl@MacBook-Pro-von-Michael.local>
2024-05-11 14:00:04 +0200

    clean up

--- src/components/playlist.tsx
+++ src/components/playlist.tsx
@@ -1,13 +1,5 @@
 'use client';
-import { invoke } from '@tauri-apps/api/core';
 
-async function getPlaylist() {
-  invoke('get_playlists');
-}
-
-export default function Playlist() {
-  getPlaylist()
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-  return <div></div>;
+export default async function Playlist() {
+  return <ul className="list-none mx-4"></ul>;
 }
